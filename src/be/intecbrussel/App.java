2fa743487dfe52d38ded4beef7749fd5c6ad9cc6
patron98 <jim.schyvens@gmail.com>
2022-03-15 11:26:21 +0100

    Second commit

--- src/be/intecbrussel/App.java
+++ src/be/intecbrussel/App.java
@@ -3,5 +3,6 @@ package be.intecbrussel;
 public class App {
     public static void main(String[] args){
         System.out.println("Hello Git");
+        System.out.println("Hello again git");
     }
 }
